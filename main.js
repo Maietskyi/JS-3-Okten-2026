4980cf3df112bc8179ad20e71c12fea41608ee97
Maietskyi <damaietskyi@gmail.com>
2026-01-31 02:02:18 +0200

    HW 1 2026

---
+++ main.js
@@ -0,0 +1,72 @@
+// Текстовий урок
+// JS. ДЗ 1
+// Коди з лекцій та коди з ДЗ
+//
+// https://github.com/OktenSchool/javascript.git
+//     ДЗ:
+//         #dYQNrBV
+//
+// Створити змінні. Присвоїти кожному з них значення: ‘hello’,’owu’,’com’, ‘ua’, 1, 10, -999, 123, 3.14, 2.7, 16, true, false.
+//
+// let hello = 'hello';
+// console.log(hello);
+// let owu = 'owu';
+// console.log(owu);
+// let com = 'com';
+// console.log(com);
+// let ua = 'ua';
+// console.log(ua);
+// let n1 = 1;
+// console.log(n1);
+// let n10 = 10;
+// console.log(n10);
+// let n999 = -999;
+// console.log(n999);
+// let n123 = 123;
+// console.log(n123);
+// let np = 3.14;
+// console.log(np);
+// let n27 = 2.7;
+// console.log(n27);
+// let n16 = 16;
+// console.log(n16);
+// let bTrue = true;
+// console.log(bTrue);
+// let bFalse = false;
+// console.log(bFalse);
+
+//     Вивести кожну змінну за допомогою: console.log
+//
+// #6Qb97gsv
+//
+// – Створити 3 змінних firstName, middleName, lastName, наповнити їх своїм ПІБ. З’єднати їх в одну змінну person (Не об’єкт, просто за допомогою конкатенації)
+//
+// let firstName = `Maietskyi`;
+// let middleName = `Mykhail`;
+// let lastName = `Mykhailovuch`;
+//
+// let person = `${firstName} ${middleName} ${lastName}`;
+// console.log(person);
+//
+// #4N0y5tufA
+//
+// – За допомогою оператора typeof визначити типи наступних змінних та вивести їх в консоль.
+//
+//     let a = 100; let b = ‘100’; let c = true;
+//
+// let a = 100;
+// let b = '100';
+// let c = true;
+// console.log(typeof a);
+// console.log(typeof b);
+// console.log(typeof c);
+//
+// #ruUtWDUI
+//
+// Додаткове для тих, хто цікавився prompt`oм
+//
+// – За допомогою 3-х різних prompt() отримати 3 слова які являються вашими Імʼям, По-батькові та роками. Та вивести в консоль
+// let name = prompt("Ваше ім'я?");
+// let firstName = prompt("Прізвище?");
+// let age = prompt("Скільки Вам років?")
+// console.log(`${name} ${firstName} ${age}`);
