6a035932179df41722fb85bb0d7a1e8d31843c81
Maietskyi <damaietskyi@gmail.com>
2026-02-01 03:15:55 +0200

    HW 3 2026 додаткові не зробив

--- main.js
+++ main.js
@@ -1,476 +1,240 @@
-// JS. ДЗ 3
-// Коди з лекцій та коди з ДЗ
+// JS. Додаткові ДЗ по масивах та циклах
+// #WpkK0ZH1
 //
-// https://github.com/OktenSchool/javascript.git
+// –створити масив з:
 //
-//     #y7crMeFwHcS
+//     – з 5 числових значень
 //
-// – За допомогою циклу for і document.write() вивести 10 блоків div c довільним текстом всередині
+// – з 5 стічкових значень
 //
-// for (let i = 0; i < 10; i++) {
-//     document.write(`<div>${+i}</div>`);
-// }
+// – з 5 значень стрічкового, числового та булевого типу
 //
-// #TYj7ncx
+// – та вивести його в консоль
 //
-// – За допомогою циклу for і document.write() вивести 10 блоків div c довільним текстом і індексом всередині
+// #4aDbSgh
 //
-// for (let i = 0; i < 10; i++) {
-//     document.write(`<div>Текст ${i}</div>`);
-// }
-//
-// #uzkt71dp
-//
-// – За допомогою циклу while вивести в документ 20 блоків h1 c довільним текстом всередині.
-//
-// let x = 0;
-// while (x < 20) {
-//     document.write(`<div>${x} Текст</div>`);
-//     x++;
-// }
-//
-//     #OeT7t3uUMFi
-//
-// – За допомогою циклу while вивести в документ 20 блоків h1 c довільним текстом і індексом всередині.
-//
-// let x = 0;
-// while (x < 20) {
-//     document.write(`<div>Index - ${x} Текст</div>`);
-//     x++;
-// }
-//
-//     #vLSZKMlO
-//
-// – Використовуючи данні з масиву, за допомоги document.write та циклу
-//
-// побудувати структуру за шаблоном
-//
-// Масив:
-//   let listOfItems = [‘html’, ‘css’, ‘javascript’, ‘mysql’, ‘mongodb’, ‘react’, ‘angular’, ‘node.js’];
+// — Створити пустий масив. Наповнити його будь-якими значеннями, звертаючись до конкретного індексу. Вивести в консоль
 //
 //
-// ШАБЛОН:
 //
-//     <ul>
 //
-//         <li>ITEM OF ARRAY</li>
 //
-//         <!–
 //
-//         і тд інші об’єкти масиву
 //
-//         …
+// #qLQLJSeN7i
 //
-//         …
+// – є масив [2,17,13,6,22,31,45,66,100,-18] :
 //
-//         …
+// 1. перебрати його циклом while
 //
-//         –>
+//     2. перебрати його циклом for
 //
-//     </ul>
+//     3. перебрати циклом while та вивести  числа тільки з непарним індексом
 //
-// замість ‘ITEM OF ARRAY’ підставити елемент з масиву, щоб вийшов цілий список з даними з масиву
+// 4. перебрати циклом for та вивести  числа тільки з непарним індексом
 //
-// let listOfItems = ['html', 'css', 'javascript', 'mysql', 'mongodb', 'react', 'angular', 'node.js'];
-// debugger
-// document.write(`<ul>`)
-// for (let item of listOfItems) {
-//     document.write(`<li>${item}`);
-// }
-// document.write(`</ul>`)
+// 5. перебрати циклом while та вивести  числа тільки парні  значення
 //
-// #Hdjws7E
+// 6. перебрати циклом for та вивести  числа тільки парні  значення
 //
-// Використовуючи данні з масиву, за допомоги document.write та циклу
+// 7. замінити кожне число, кратне 3, на слово “okten”
 //
-// побудувати структуру по шаблону
+// 8. вивести масив у зворотньому порядку.
 //
-// Великими літерами прописанні властивості об’єкту які потрібно впровадити в шаблон
+// 9. всі попередні завдання (окрім 8), але у зворотньому циклі (задом наперед)
 //
-// let products = [
 //
-//     {
 //
-//         title: ‘milk’,
+// #yHAwJOyiC
 //
-// price: 22,
+// – Створити масив з 10 числових елементів. Вивести в консоль всі його елементи в циклі.
 //
-//     image: ‘https://www.mcqueensdairies.co.uk/wp-content/uploads/2019/02/Mcqueens_1litre_whole_organic-300×300-3.jpg’
+//     #GamKju89ob
 //
-//     },
+// – Створити масив з 10 строкових елементів. Вивести в консоль всі його елементи в циклі.
 //
-// {
+//     #Bm76xmg
 //
-//     title: ‘juice’,
+// – Створити масив з 10 елементів будь-якого типу. Вивести в консоль всі його елементи в циклі.
 //
-//     price: 27,
+//     #u3vmD0YJXh
 //
-//         image: ‘https://images-na.ssl-images-amazon.com/images/I/61jL2GCuKLL._SX679_PIbundle-24,TopRight,0,0_AA679SH20_.jpg’
+// – Створити масив з 10 елементів числового, стрічкового і булевого типу. За допомогою if та typeof вивести тільки булеві елементи
 //
-// },
+// #9stMq2ou
 //
-// {
+// – Створити масив з 10 елементів числового, стрічкового і булевого типу. За допомогою if та typeof вивести тільки числові елементи
 //
-//     title: ‘tomato’,
+// #mK4pmM4
 //
-//     price: 47,
+// – Створити масив з 10 елементів числового, стрічкового і булевого типу. За допомогою if та typeof вивести тільки рядкові елементи
 //
-//         image: ‘https://dictionary.cambridge.org/ru/images/thumb/tomato_noun_001_17860.jpg?version=5.0.74’
 //
-// },
 //
-// {
+// #0pm3EyTKy9
 //
-//     title: ‘tea’,
+// – Створити порожній масив. Наповнити його 10 елементами (різними за типами) через звернення до конкретних індексів. Вивести в консоль всі його елементи в циклі.
 //
-//     price: 15,
+//     #mDMWMW5a
 //
-//         image: ‘https://yogiproducts.com/wp-content/uploads/2009/03/YT-US-CAR-RelaxedMind-C23-202201-V2-3DFront_withGlow-300DPI-1.png’
+// – Створити цикл for на 10  ітерацій з кроком 1. Вивести поточний номер кроку через console.log та document.write
 //
-// },
+// #4sXhaa5YMM
+//
+// – Створити цикл for на 100 ітерацій з кроком 1. Вивести поточний номер кроку через console.log та document.write
+//
+// #s24slNyz7
+//
+// – Створити цикл for на 100 ітерацій з кроком 2. Вивести поточний номер кроку через console.log та document.write
+//
+// #zananT5FR1
+//
+// – Створити цикл for на 100 ітерацій. Вивести тільки парні кроки. через console.log + document.write
+//
+// #Tfrwls7FM
+//
+// – Створити цикл for на 100 ітерацій. Вивести тільки непарні кроки. через console.log + document.write
+//
+//
+//
+//
+//
+// #reLkOkTB29Q
+//
+// створити масив книжок (назва, кількість сторінок, автори , жанри).
+//
+// – знайти найбільшу книжку.
+//
+// – знайти книжку/ки з найбільшою кількістю жанрів
+//
+// – знайти книжку/ки з найдовшою назвою
+//
+// – знайти книжку/ки, які писали 2 автори
+//
+// – знайти книжку/ки, які писав 1 автор
+//
+//
+//
+// 1. Створити пустий масив та :
+//
+//     a. заповнити його 50 парними числами за допомоги циклу.
+//
+//     b. заповнити його 50 непарними числами за допомоги циклу.
+//
+//     c. Заповнити масив 20-ма рандомними числами. (Google: Generate random number JS)
+//
+// d. Заповнити масив 20-ма рандомними числами в діапазоні від 8 до 732 (Google: Generate random number JS)
+//
+// 2. Вивести за допомогою console.log кожен третій елемент
+//
+// 3. Вивести за допомогою console.log кожен третій елемент, тільки якщо цей елемент є парним.
+//
+// 4. Вивести за допомогою console.log кожен третій елемент, тільки якщо цей елемент є парним, та записати їх в новий масив
+//
+// 5. Вивести кожен елемент масиву, сусід справа якого є парним
+//
+// EXAMPLE: [ 1, 2, 3, 5, 7, 9, 56, 8, 67 ] -> Має бути виведено 1, 9, 56
+//
+// 6. Є масив з числами [100,250,50,168,120,345,188], Які характеризують вартість окремої покупки. Обрахувати середній чек.
+//
+//
+//
+// 7. Створити масив з рандомними значеннями, помножити всі його елементи на 5 та перемістити їх в інший масив.
+//
+// 8. Створити масив з будь-якими значеннями (стрінги, числа, і тд…). пройтись по ньому, і, якщо елемент є числом, додати його в інший масив.
+//
+//
+//
+//
+//
+//
+//
+// – Дано 2 масиви з рівною кількістю об’єктів.
+//
+//     Масиви:
+//
+// let usersWithId = [
+//
+//     {id: 1, name: ‘vasya’, age: 31, status: false},
+//
+// {id: 2, name: ‘petya’, age: 30, status: true},
+//
+// {id: 3, name: ‘kolya’, age: 29, status: true},
+//
+// {id: 4, name: ‘olya’, age: 28, status: false}
 //
 // ];
 //
 //
 //
-// ШАБЛОН
+// let citiesWithId = [
 //
-// <div class=”product-card”>
+//     {user_id: 3, country: ‘USA’, city: ‘Portland’},
 //
-// <h3 class=”product-title”>TITLE. Price – PRICE</h3>
+// {user_id: 1, country: ‘Ukraine’, city: ‘Ternopil’},
 //
-// <img src=”IMAGE” alt=”” class=”product-image”>
+// {user_id: 2, country: ‘Poland’, city: ‘Krakow’},
 //
-// </div>
+// {user_id: 4, country: ‘USA’, city: ‘Miami’}
 //
-// Замість TITLE PRICE IMAGE – підставити відповідні поля з об’єкту
-//
-// let products = [
-//     {
-//         title: 'milk',
-//         price: 22,
-//         image: 'https://www.oldenburger-dairy.com/media/00/6c/e1/1737642194/11067_1l_3_5_410x410_full_cream_milk.png'
-//     },
-//     {
-//         title: 'juice',
-//         price: 27,
-//         image: 'https://images-na.ssl-images-amazon.com/images/I/61jL2GCuKLL._SX679_PIbundle-24,TopRight,0,0_AA679SH20_.jpg'
-//     },
-//     {
-//         title: 'tomato',
-//         price: 47,
-//         image: 'https://dictionary.cambridge.org/ru/images/thumb/tomato_noun_001_17860.jpg?version=5.0.74'
-//     },
-//     {
-//         title: 'tea',
-//         price: 15,
-//         image: 'https://yogiproducts.com/wp-content/uploads/2009/03/YT-US-CAR-RelaxedMind-C23-202201-V2-3DFront_withGlow-300DPI-1.png'
-//     },
 // ];
 //
-// for (let product of products) {
-//     document.write(`<div class="product-card">
-//     <h3 class="product-title">${product.title}. Price – ${product.price}</h3>
-//     <img src=${product.image} alt='Фото' class="product-image">
-// </div>`)
+//
+//
+// З’єднати в один об’єкт користувача та місто з відповідними “id” та “user_id” .
+//
+// Записати цей об’єкт в новий масив
+//
+// Example:
+//
+//     let usersWithCities = [
+//
+//         {
+//
+//             id: 1, // <===
+//
+//             name: ‘vasya’,
+//
+// age: 31,
+//
+//     status: false,
+//
+//     address: {
+//
+//     user_id: 1, // <===
+//
+//         country: ‘Ukraine’,
+//
+//     city: ‘Ternopil’
+//
 // }
 //
-// #4WrHwFTEop0
+// },
 //
-// є масив
+// // TO BE CONTINUED …..
 //
-let users = [
-    {name: 'vasya', age: 31, status: false},
-    {name: 'petya', age: 30, status: true},
-    {name: 'kolya', age: 29, status: true},
-    {name: 'olya', age: 28, status: false},
-    {name: 'max', age: 30, status: true},
-    {name: 'anya', age: 31, status: false},
-    {name: 'oleg', age: 28, status: false},
-    {name: 'andrey', age: 29, status: true},
-    {name: 'masha', age: 30, status: true},
-    {name: 'olya', age: 31, status: false},
-    {name: 'max', age: 31, status: true}
-];
-//
-// за допомогою циклу вивести:
-//
-//     – користувачів зі статусом true
-//
-// for (let user of users) {
-//     if (user.status) {
-//         document.write(`<div>${user.name} - ${user.age}, ${user.status}</div>`);
-//     }
-// }
-//  – користувачів зі статусом false
-//
-// for (let user of users) {
-//     if (!user.status) {
-//         document.write(`<div>${user.name} - ${user.age}, ${user.status}</div>`)
-//     }
-// }
-//  – користувачів, які старші за 30 років
-//
-// debugger
-// for (let user of users) {
-//     if (user.age > 30) {
-//         document.write(`<div>${user.name} - ${user.age}, ${user.status}</div>`);
-//     }
-// }
-// =============
-//
-//     За допомогою циклу вивести всю інформацію про користувачів, використовуючи шаблон
+// ]
 //
 //
-// за допомогою стилів, побудувати сітку по 2 об’єктах в рядок
 //
-// Матеріали уроку
 //
-let usersList = [
-    {
-        id: 1,
-        name: 'Leanne Graham',
-        username: 'Bret',
-        email: 'Sincere@april.biz',
-        address: {
-            street: 'Kulas Light',
-            suite: 'Apt. 556',
-            city: 'Gwenborough',
-            zipcode: '92998-3874',
-            geo: {
-                lat: '-37.3159',
-                lng: '81.1496'
-            }
-        },
-        phone: '1-770-736-8031 x56442',
-        website: 'hildegard.org',
-        company: {
-            name: 'Romaguera-Crona',
-            catchPhrase: 'Multi-layered client-server neural-net',
-            bs: 'harness real-time e-markets'
-        }
-    },
-    {
-        id: 2,
-        name: 'Ervin Howell',
-        username: 'Antonette',
-        email: 'Shanna@melissa.tv',
-        address: {
-            street: 'Victor Plains',
-            suite: 'Suite 879',
-            city: 'Wisokyburgh',
-            zipcode: '90566-7771',
-            geo: {
-                lat: '-43.9509',
-                lng: '-34.4618'
-            }
-        },
-        phone: '010-692-6593 x09125',
-        website: 'anastasia.net',
-        company: {
-            name: 'Deckow-Crist',
-            catchPhrase: 'Proactive didactic contingency',
-            bs: 'synergize scalable supply-chains'
-        }
-    },
-    {
-        id: 3,
-        name: 'Clementine Bauch',
-        username: 'Samantha',
-        email: 'Nathan@yesenia.net',
-        address: {
-            street: 'Douglas Extension',
-            suite: 'Suite 847',
-            city: 'McKenziehaven',
-            zipcode: '59590-4157',
-            geo: {
-                lat: '-68.6102',
-                lng: '-47.0653'
-            }
-        },
-        phone: '1-463-123-4447',
-        website: 'ramiro.info',
-        company: {
-            name: 'Romaguera-Jacobson',
-            catchPhrase: 'Face to face bifurcated interface',
-            bs: 'e-enable strategic applications'
-        }
-    },
-    {
-        id: 4,
-        name: 'Patricia Lebsack',
-        username: 'Karianne',
-        email: 'Julianne.OConner@kory.org',
-        address: {
-            street: 'Hoeger Mall',
-            suite: 'Apt. 692',
-            city: 'South Elvis',
-            zipcode: '53919-4257',
-            geo: {
-                lat: '29.4572',
-                lng: '-164.2990'
-            }
-        },
-        phone: '493-170-9623 x156',
-        website: 'kale.biz',
-        company: {
-            name: 'Robel-Corkery',
-            catchPhrase: 'Multi-tiered zero tolerance productivity',
-            bs: 'transition cutting-edge web services'
-        }
-    },
-    {
-        id: 5,
-        name: 'Chelsey Dietrich',
-        username: 'Kamren',
-        email: 'Lucio_Hettinger@annie.ca',
-        address: {
-            street: 'Skiles Walks',
-            suite: 'Suite 351',
-            city: 'Roscoeview',
-            zipcode: '33263',
-            geo: {
-                lat: '-31.8129',
-                lng: '62.5342'
-            }
-        },
-        phone: '(254)954-1289',
-        website: 'demarco.info',
-        company: {
-            name: 'Keebler LLC',
-            catchPhrase: 'User-centric fault-tolerant solution',
-            bs: 'revolutionize end-to-end systems'
-        }
-    },
-    {
-        id: 6,
-        name: 'Mrs. Dennis Schulist',
-        username: 'Leopoldo_Corkery',
-        email: 'Karley_Dach@jasper.info',
-        address: {
-            street: 'Norberto Crossing',
-            suite: 'Apt. 950',
-            city: 'South Christy',
-            zipcode: '23505-1337',
-            geo: {
-                lat: '-71.4197',
-                lng: '71.7478'
-            }
-        },
-        phone: '1-477-935-8478 x6430',
-        website: 'ola.org',
-        company: {
-            name: 'Considine-Lockman',
-            catchPhrase: 'Synchronised bottom-line interface',
-            bs: 'e-enable innovative applications'
-        }
-    },
-    {
-        id: 7,
-        name: 'Kurtis Weissnat',
-        username: 'Elwyn.Skiles',
-        email: 'Telly.Hoeger@billy.biz',
-        address: {
-            street: 'Rex Trail',
-            suite: 'Suite 280',
-            city: 'Howemouth',
-            zipcode: '58804-1099',
-            geo: {
-                lat: '24.8918',
-                lng: '21.8984'
-            }
-        },
-        phone: '210.067.6132',
-        website: 'elvis.io',
-        company: {
-            name: 'Johns Group',
-            catchPhrase: 'Configurable multimedia task-force',
-            bs: 'generate enterprise e-tailers'
-        }
-    },
-    {
-        id: 8,
-        name: 'Nicholas Runolfsdottir V',
-        username: 'Maxime_Nienow',
-        email: 'Sherwood@rosamond.me',
-        address: {
-            street: 'Ellsworth Summit',
-            suite: 'Suite 729',
-            city: 'Aliyaview',
-            zipcode: '45169',
-            geo: {
-                lat: '-14.3990',
-                lng: '-120.7677'
-            }
-        },
-        phone: '586.493.6943 x140',
-        website: 'jacynthe.com',
-        company: {
-            name: 'Abernathy Group',
-            catchPhrase: 'Implemented secondary concept',
-            bs: 'e-enable extensible e-tailers'
-        }
-    },
-    {
-        id: 9,
-        name: 'Glenna Reichert',
-        username: 'Delphine',
-        email: 'Chaim_McDermott@dana.io',
-        address: {
-            street: 'Dayna Park',
-            suite: 'Suite 449',
-            city: 'Bartholomebury',
-            zipcode: '76495-3109',
-            geo: {
-                lat: '24.6463',
-                lng: '-168.8889'
-            }
-        },
-        phone: '(775)976-6794 x41206',
-        website: 'conrad.com',
-        company: {
-            name: 'Yost and Sons',
-            catchPhrase: 'Switchable contextually-based project',
-            bs: 'aggregate real-time technologies'
-        }
-    },
-    {
-        id: 10,
-        name: 'Clementina DuBuque',
-        username: 'Moriah.Stanton',
-        email: 'Rey.Padberg@karina.biz',
-        address: {
-            street: 'Kattie Turnpike',
-            suite: 'Suite 198',
-            city: 'Lebsackbury',
-            zipcode: '31428-2261',
-            geo: {
-                lat: '-38.2386',
-                lng: '57.2232'
-            }
-        },
-        phone: '024-648-3804',
-        website: 'ambrose.net',
-        company: {
-            name: 'Hoeger LLC',
-            catchPhrase: 'Centralized empowering task-force',
-            bs: 'target end-to-end models'
-        }
-    }
-];
 //
-document.write(`<div class="users-box">`)
-for (let user of usersList) {
-    document.write(`<div class="user-block">
-        <h2>${user.id} – ${user.name} – ${user.username} </h2>
-        <h3>${user.email} – ${user.phone}</h3>
-        <div class="address-block">
-            <p>City – ${user.address.city}</p>
-            <p>Street – ${user.address.street}</p>
-            <p>Suite – ${user.address.suite}</p>
-            <p>Zip code – ${user.address.zipcode}</p>
-        </div>
-    </div>`)
-}
-document.write(`</div>`)
-
+//
+//
+//
+//
+// – Взяти масив з 10 чисел або створити його. Вивести в консоль тільки ті елементи, значення яких є парними.
+//
+//
+//
+// – Взяти масив з 10 чисел або створити його. Створити 2-й порожній масив. За допомогою будь-якого циклу скопіювати значення одного масиву в інший.
+//
+//
+//
+//
+//
+// – Дано масив: [ ‘a’, ‘b’, ‘c’] . За допомогою циклу for зібрати всі букви в слово.
+//
+// – Дано масив: [ ‘a’, ‘b’, ‘c’] . За допомогою циклу while зібрати всі букви в слово.
+//
+// – Дано масив: [ ‘a’, ‘b’, ‘c’] . За допомогою циклу for of зібрати всі букви в слово.
