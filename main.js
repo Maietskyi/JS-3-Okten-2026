4f049d2de0611d42839f2f7aeb9e6ab346b7fb55
Maietskyi <damaietskyi@gmail.com>
2026-02-01 22:29:29 +0200

    HW 3 2026 додаткові до 537 рядка

--- main.js
+++ main.js
@@ -30,7 +30,7 @@
 // #qLQLJSeN7i
 //
 // – є масив [2,17,13,6,22,31,45,66,100,-18] :
-let arr = [2, 17, 13, 6, 22, 31, 45, 66, 100, -18];
+// let arr = [2, 17, 13, 6, 22, 31, 45, 66, 100, -18];
 //
 // 1. перебрати його циклом while
 // let i = 0;
@@ -186,80 +186,355 @@ let arr = [2, 17, 13, 6, 22, 31, 45, 66, 100, -18];
 // #yHAwJOyiC
 //
 // – Створити масив з 10 числових елементів. Вивести в консоль всі його елементи в циклі.
+// let arr = [52, 54, 36, 895, 72, 145, 69, 321, 754, 625]
+// for (let i = 0; i < arr.length; i++) {
+//     console.log(arr[i])
+// }
+// let i = 0;
+// while (i < arr.length) {
+//     console.log(arr[i]);
+//     i++
+// }
+// for (let i of arr){
+//     console.log(i);
+// }
 //
 //     #GamKju89ob
 //
 // – Створити масив з 10 строкових елементів. Вивести в консоль всі його елементи в циклі.
+// let arrS = ['Misha', 'Okten', 'Maietskyi', 'school', 'Passat', 'Business', 'Many', 'Lviv', 'Ukraine', 'Milions']
+//
+// for (let i = 0; i < arrS.length; i++) {
+//     console.log(arrS[i])
+// }
+//
+// let i = 0;
+// while (i < arrS.length) {
+//     console.log(arrS[i]);
+//     i++
+// }
+// for (let i of arrS){
+//     console.log(i)
+// }
 //
 //     #Bm76xmg
 //
 // – Створити масив з 10 елементів будь-якого типу. Вивести в консоль всі його елементи в циклі.
+// let arrM = [654, true, 'Misha', false, 456, 6892, 'Okten', 'Maietskyi', 23.2, 'Mama']
+// for (let i of arrM) {
+//     console.log(i)
+// }
+// for (let i = 0; i < arrM.length; i++ ){
+//     console.log(arrM[i])
+// }
+// let i = 0;
+// while (i<arrM.length){
+//     console.log(arrM[i])
+//     i++
+// }
 //
 //     #u3vmD0YJXh
 //
 // – Створити масив з 10 елементів числового, стрічкового і булевого типу. За допомогою if та typeof вивести тільки булеві елементи
+// let arrM = [654, true, 'Misha', false, 456, 6892, 'Okten', 'Maietskyi', 23.2, 'Mama']
+// for (let i of arrM) {
+//     if(typeof(i) === "boolean"){
+//         console.log(i);
+//     }
+// }
+// for (let i = 0; i < arrM.length; i++) {
+//     if (typeof arrM[i] === 'boolean') {
+//         console.log(arrM[i])
+//     }
+// }
+// let i = 0;
+// while (i < arrM.length) {
+//     if (typeof arrM[i] === 'boolean') {
+//         console.log(arrM[i]);
+//     }
+//     i++
+// }
 //
 // #9stMq2ou
 //
 // – Створити масив з 10 елементів числового, стрічкового і булевого типу. За допомогою if та typeof вивести тільки числові елементи
+// let arrM = [654, true, 'Misha', false, 456, 6892, 'Okten', 'Maietskyi', 23.2, 'Mama']
+// for (let i = 0; i < arrM.length; i++) {
+//     if (typeof arrM[i] === 'number') {
+//         console.log(arrM[i])
+//     }
+// }
+// for (let i of arrM) {
+//     if (typeof (i) === 'number') {
+//         console.log(i)
+//     }
+// }
+// let i = 0;
+// while (i < arrM.length) {
+//     if (typeof arrM[i] === 'number') {
+//         console.log(arrM[i]);
+//     }
+//     i++
+// }
 //
 // #mK4pmM4
 //
 // – Створити масив з 10 елементів числового, стрічкового і булевого типу. За допомогою if та typeof вивести тільки рядкові елементи
 //
-//
+// let arrM = [654, true, 'Misha', false, 456, 6892, 'Okten', 'Maietskyi', 23.2, 'Mama']
+// for (let i = 0; i < arrM.length; i++) {
+//     if (typeof arrM[i] === 'string') {
+//         console.log(arrM[i])
+//     }
+// }
+// for (let i of arrM) {
+//     if (typeof i === 'string'){
+//         console.log(i)
+//     }
+// }
+// let i = 0;
+// while (i < arrM.length) {
+//     if (typeof arrM[i] === 'string') {
+//         console.log(arrM[i]);
+//     }
+//     i++
+// }
 //
 // #0pm3EyTKy9
 //
 // – Створити порожній масив. Наповнити його 10 елементами (різними за типами) через звернення до конкретних індексів. Вивести в консоль всі його елементи в циклі.
+// let arr = [];
+// arr[0] = 654
+// arr[2] = true
+// arr[4] = 'Misha'
+// arr[6] = false
+// arr[8] = 456
+// arr[9] = 6892
+// arr[7] = 'Okten'
+// arr[5] = 'Maietskyi'
+// arr[3] = 23.2
+// arr[1] = 'Mama'
+// console.log(arr)
 //
 //     #mDMWMW5a
 //
 // – Створити цикл for на 10  ітерацій з кроком 1. Вивести поточний номер кроку через console.log та document.write
+// for (i = 0; i < 10; i++) {
+//     console.log(`Крок № ${i+1}`)
+//     document.write(`<div>Крок № ${i+1}</div>`)
+// }
+// let i = 0;
+// while (i < 10) {
+//     console.log(`Крок № ${i+1}`);
+//     document.write(`<div>Крок № ${i+1}</div>`)
+//     i++
+// }
 //
 // #4sXhaa5YMM
 //
 // – Створити цикл for на 100 ітерацій з кроком 1. Вивести поточний номер кроку через console.log та document.write
+// for (let i = 0; i < 100; i++) {
+//     console.log(`Крок № ${i+1}`)
+//     document.write(`<div>Крок № ${i+1}</div>`)
+// }
+// let i = 0;
+// while (i < 100) {
+//     console.log(`Крок № ${i+1}`)
+//     document.write(`<div>Крок № ${++i}</div>`)
+// }
 //
 // #s24slNyz7
 //
 // – Створити цикл for на 100 ітерацій з кроком 2. Вивести поточний номер кроку через console.log та document.write
+// for (let i = 0; i < 100; i+=2) {
+//     console.log(`Крок № ${i}`)
+//     document.write(`<div>Крок № ${i}</div>`)
+// }
+// let i = 0;
+// while (i < 100) {
+//     console.log(i);
+//     document.write(`<p>Крок № ${i}</p>`)
+//     i+=2
+// }
 //
 // #zananT5FR1
 //
 // – Створити цикл for на 100 ітерацій. Вивести тільки парні кроки. через console.log + document.write
+// for (i = 0; i < 100; i++) {
+//     if (i%2===0){
+//         console.log(i);
+//         document.write(`<p>Крок № ${i}</p>`);
+//     }
+// }
+// let i = 0;
+// while (i < 100) {
+//     if (i % 2 === 0) {
+//         console.log(`Крок № ${i}`)
+//         document.write(`<p>Крок № ${i}</p>`)
+//     }
+//     i++
+// }
 //
 // #Tfrwls7FM
 //
 // – Створити цикл for на 100 ітерацій. Вивести тільки непарні кроки. через console.log + document.write
-//
-//
-//
-//
+// for (let i = 0; i < 100; i++) {
+//     if (i % 2 !== 0) {
+//         console.log(i)
+//         document.write(`<p>Крок № ${i}</p>`)
+//     }
+// }
+// let i = 0;
+// while (i < 100){
+//     if(i % 2 !== 0) {
+//         console.log(i);
+//         document.write(`<p>Крок № ${i}</p>`)
+//     }
+//     i++
+// }
 //
 // #reLkOkTB29Q
 //
 // створити масив книжок (назва, кількість сторінок, автори , жанри).
+let books = [
+    {
+        title: "Тіні забутих предків",
+        pages: 160,
+        authors: ["Михайло Коцюбинський"],
+        genres: ["драма", "історичний роман"]
+    },
+    {
+        title: "Гаррі Поттер і філософський камінь",
+        pages: 320,
+        authors: ["Джоан Роулінг"],
+        genres: ["фентезі", "пригоди"]
+    },
+    {
+        title: "Майстер і Маргарита",
+        pages: 400,
+        authors: ["Михайло Булгаков", "Райс"],
+        genres: ["містика", "фантастика", "сатира"]
+    },
+    {
+        title: "Сто років самотності",
+        pages: 450,
+        authors: ["Габрієль Гарсія Маркес"],
+        genres: ["магічний реалізм", "роман",]
+    },
+    {
+        title: "Кобзар",
+        pages: 250,
+        authors: ["Тарас Шевченко", "Українка", "Франко"],
+        genres: ["поезія", "класика"]
+    },
+    {
+        title: "1984",
+        pages: 328,
+        authors: ["Джордж Орвелл"],
+        genres: ["антиутопія", "політична фантастика"]
+    },
+    {
+        title: "Портрет Доріана Грея",
+        pages: 310,
+        authors: ["Оскар Вайльд"],
+        genres: ["роман", "філософська проза"]
+    },
+    {
+        title: "Злочин і кара",
+        pages: 430,
+        authors: ["Федір Достоєвський"],
+        genres: ["роман", "драма", "психологія", "автобіографія"]
+    },
+    {
+        title: "Пригоди Тома Сойєра",
+        pages: 290,
+        authors: ["Марк Твен"],
+        genres: ["пригоди", "гумор", "дитяча література"]
+    },
+    {
+        title: "Лісова пісня",
+        pages: 180,
+        authors: ["Леся Українка"],
+        genres: ["драма-феєрія", "поезія", "українська класика"]
+    }
+];
 //
 // – знайти найбільшу книжку.
 //
+// let pageBig = books[0];
+// for (let i = 0; i < books.length; i++) {
+//     let book = books[i];
+//     if (pageBig.pages < book.pages) {
+//         pageBig = book;
+//     }
+// }
+// console.log(pageBig);
+// document.write(`<p>${pageBig.title}</p>`);
+//
 // – знайти книжку/ки з найбільшою кількістю жанрів
+// let book = books[0];
+// for (let i = 0; i < books.length; i++) {
+//     if (book.genres.length < books[i].genres.length) {
+//         book = books[i];
+//     }
+// }
+// console.log(book)
+// document.write(`<p>${book.title}</p>`);
 //
 // – знайти книжку/ки з найдовшою назвою
+// let book = books[0];
+// for (let i = 0; i < books.length; i++) {
+//     if (book.title.length < books[i].title.length) {
+//         book = books[i];
+//     }
+// }
+// console.log(book)
+// document.write(`<p>${book.title}</p>`)
 //
 // – знайти книжку/ки, які писали 2 автори
+// let book = books[0];
+// for (let i = 0; i < books.length; i++) {
+//     if (book.authors.length < books[i].authors.length) {
+//         book = books[i];
+//     }
+// }
+// console.log(book)
+// document.write(`<p>${book.title} - ${book.authors}</p>`)
 //
 // – знайти книжку/ки, які писав 1 автор
-//
-//
+// let books1 = []
+// for (let i = 0; i < books.length; i++) {
+//     if (books[i].authors.length === 1) {
+//         books1.push(books[i]);
+//         document.write(`<p>${books[i].title}</p>`)
+//     }
+// }
+// console.log(books1)
 //
 // 1. Створити пустий масив та :
+let arr = [];
 //
 //     a. заповнити його 50 парними числами за допомоги циклу.
+// for (let i = 0; arr.length < 50; i++) {
+//     if (i % 2 === 0) {
+//         arr.push(i);
+//     }
+// }
+// console.log(arr)
 //
 //     b. заповнити його 50 непарними числами за допомоги циклу.
+// for (let i = 0; arr.length < 50; i++) {
+//     if (i % 2 !== 0) {
+//         arr.push(i);
+//     }
+// }
+// console.log(arr)
 //
 //     c. Заповнити масив 20-ма рандомними числами. (Google: Generate random number JS)
+// for (let i = 0; arr.length < 20; i++) {
+//     arr.push(Math.floor(Math.random() * 1000));
+// }
+// console.log(arr)
 //
 // d. Заповнити масив 20-ма рандомними числами в діапазоні від 8 до 732 (Google: Generate random number JS)
 //
