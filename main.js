036947b968d01acc2f68d8cd3f6cca2e67e1498e
Maietskyi <damaietskyi@gmail.com>
2026-02-05 01:56:39 +0200

    HW 6 2026 ряд 349

--- main.js
+++ main.js
@@ -144,12 +144,6 @@ let coursesAndDurationArray = [
 //     .map((value, index) => ({id: index + 1, ...value}));
 //
 // console.log(course);
-
-// let course = coursesAndDurationArray.sort((a, b) => a.monthDuration - b.monthDuration)
-//     .filter(value => value.monthDuration > 5)
-//     .map((value, index) => ({id: index, ...value}));
-//
-// console.log(course);
 //
 //  — відсортувати його за спаданням за monthDuration
 //
@@ -157,17 +151,202 @@ let coursesAndDurationArray = [
 //
 //  — за допомоги map перетворити кожен елемент на наступний тип {id,title,monthDuration}
 //
+// let course = coursesAndDurationArray.sort((a, b) => a.monthDuration - b.monthDuration)
+//     .filter(value => value.monthDuration > 5)
+//     .map((value, index) => ({id: index, ...value}));
+//
+// console.log(course);
+//
 // #4LJn7zBx
 //
 // взяти з arrays.js масив coursesArray
 //
+// let coursesArray = [
+//     {
+//         title: 'JavaScript Complex',
+//         monthDuration: 5,
+//         hourDuration: 909,
+//         modules: ['html', 'css', 'js', 'mysql', 'mongodb', 'react', 'angular', 'aws', 'docker', 'git', 'node.js']
+//     },
+//     {
+//         title: 'Java Complex',
+//         monthDuration: 6,
+//         hourDuration: 909,
+//         modules: ['html',
+//             'css',
+//             'js',
+//             'mysql',
+//             'mongodb',
+//             'angular',
+//             'aws',
+//             'docker',
+//             'git',
+//             'java core',
+//             'java advanced']
+//     },
+//     {
+//         title: 'Python Complex',
+//         monthDuration: 6,
+//         hourDuration: 909,
+//         modules: ['html',
+//             'css',
+//             'js',
+//             'mysql',
+//             'mongodb',
+//             'angular',
+//             'aws',
+//             'docker',
+//             'python core',
+//             'python advanced']
+//     },
+//     {
+//         title: 'QA Complex',
+//         monthDuration: 4,
+//         hourDuration: 909,
+//         modules: ['html', 'css', 'js', 'mysql', 'mongodb', 'git', 'QA/QC']
+//     },
+//     {
+//         title: 'FullStack',
+//         monthDuration: 7,
+//         hourDuration: 909,
+//         modules: ['html',
+//             'css',
+//             'js',
+//             'mysql',
+//             'mongodb',
+//             'react',
+//             'angular',
+//             'aws',
+//             'docker',
+//             'git',
+//             'node.js',
+//             'python',
+//             'java']
+//     },
+//     {
+//         title: 'Frontend',
+//         monthDuration: 4,
+//         hourDuration: 909,
+//         modules: ['html', 'css', 'js', 'mysql', 'mongodb', 'react', 'angular', 'aws', 'docker', 'git', 'sass']
+//     }
+// ];
+//
 // –написати пошук всіх об’єктів, в яких в modules є sass
+//
+// console.log(coursesArray.filter(value => value.modules.includes('sass')));
+// console.log(coursesArray.filter(value => value.modules.includes('js')));
+// console.log(coursesArray.filter(value => value.modules.includes('docker')));
+//
+// let sorte = (arr) => arr.filter(item => item.modules.includes('sass'));
+// console.log(sorte(coursesArray));
 //
 // –написати пошук всіх об’єктів, в яких в modules є docker
 //
+// let sortDoc = (arr) => arr.filter(item => item.modules.includesAll('docker'));
+// console.log(sorte(coursesArray));
 //
+// let courses = [
+//     {
+//         title: 'JavaScript Complex',
+//         monthDuration: 5,
+//         hourDuration: 909,
+//         modules: ['html', 'css', 'js', 'mysql', 'mongodb', 'react', 'angular', 'aws', 'docker', 'git', 'node.js'],
+//         logo: '',
+//         price: 0,
+//         rating: 5,
+//         avgResult: 99
+//     },
+//     {
+//         title: 'Java Complex',
+//         monthDuration: 6,
+//         hourDuration: 909,
+//         modules: ['html',
+//             'css',
+//             'js',
+//             'mysql',
+//             'mongodb',
+//             'react',
+//             'angular',
+//             'aws',
+//             'docker',
+//             'git',
+//             'java core',
+//             'java advanced'],
+//         logo: '',
+//         price: 0,
+//         rating: 4.998,
+//         avgResult: 97
+//     },
+//     {
+//         title: 'Python Complex',
+//         monthDuration: 6,
+//         hourDuration: 909,
+//         modules: ['html',
+//             'css',
+//             'js',
+//             'mysql',
+//             'mongodb',
+//             'react',
+//             'angular',
+//             'aws',
+//             'docker',
+//             'git',
+//             'python core',
+//             'python advanced'],
+//         logo: '',
+//         price: 0,
+//         rating: 4.812,
+//         avgResult: 98
+//     },
+//     {
+//         title: 'QA Complex',
+//         monthDuration: 4,
+//         hourDuration: 909,
+//         modules: ['html', 'css', 'js', 'mysql', 'mongodb', 'react', 'angular', 'aws', 'docker', 'git', 'QA/QC'],
+//         logo: '',
+//         price: 0,
+//         rating: 4.65,
+//         avgResult: 97
+//     },
+//     {
+//         title: 'FullStack',
+//         monthDuration: 7,
+//         hourDuration: 909,
+//         modules: ['html',
+//             'css',
+//             'js',
+//             'mysql',
+//             'mongodb',
+//             'react',
+//             'angular',
+//             'aws',
+//             'docker',
+//             'git',
+//             'node.js',
+//             'python',
+//             'java'],
+//         logo: '',
+//         price: 0,
+//         rating: 4.772,
+//         avgResult: 100
+//     },
+//     {
+//         title: 'Frontend',
+//         monthDuration: 4,
+//         hourDuration: 909,
+//         modules: ['html', 'css', 'js', 'mysql', 'mongodb', 'react', 'angular', 'aws', 'docker', 'git', 'sass'],
+//         logo: '',
+//         price: 0,
+//         rating: 4.53,
+//         avgResult: 90
+//     }
+// ];
 //
-//
+// console.log(courses.filter(value => value.modules.includes('html')));
+// console.log(courses.filter(value => value.modules.includes('css')));
+// console.log(courses.filter(value => value.modules.includes('js')));
+// console.log(courses.filter(value => value.modules.includes('mysql')));
+// console.log(courses.filter(value => value.modules.includes('mongodb')));
 //
 // #bolvdlhP
 //
