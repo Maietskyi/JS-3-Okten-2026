4e4076d4bb7bb13599f3a2742b154f69982741dc
Maietskyi <damaietskyi@gmail.com>
2026-03-08 23:45:35 +0200

    HW 10  73

--- main.js
+++ main.js
@@ -57,11 +57,20 @@
 //
 // є сторінка, на якій є блок, в кому знаходиться цифра. Написати код, який при кожному перезавантажені сторінки буде додавати до неї +1
 //
+// let num = +localStorage.getItem('number');
+// num += 1;
+// localStorage.setItem('number', num);
+//
+// document.getElementById('text').innerText = num;
 //
 // #LhSfdhM3
 //
 // Є сторінка index.html (назва довільна), при відвідуванні якої в локальне сховище, в масив sessionsList зберігається інформація про дату та час відвідування сторінки. Є  сторінка sessionsListPage.html (назва довільна), при відвідуванні якої потрібно відмалювати всю інформацію про відвідування сторінки index.html. Інфу НЕ виводити в консоль, а малювати в DOM
 //
+// let sessionsList = JSON.parse(localStorage.getItem('sessionsList')) || [];
+// sessionsList.push(new Date());
+// console.log(sessionStorage);
+// localStorage.setItem('sessionsList', JSON.stringify(sessionsList));
 //
 // #Jg0gPO00
 //
